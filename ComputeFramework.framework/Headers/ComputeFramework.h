5a53ee66d913abb880508f57d1c1a2b8630aabb7
Pradeep Rajkumar <rajkumar.pradeep@yahoo.com>
2018-03-21 19:51:36 +0530

    Added the base framework.

---
+++ ComputeFramework.framework/Headers/ComputeFramework.h
@@ -0,0 +1,19 @@
+//
+//  ComputeFramework.h
+//  ComputeFramework
+//
+//  Created by Pradeep Rajkumar on 20/03/18.
+//  Copyright © 2018 Pradeep Rajkumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ComputeFramework.
+FOUNDATION_EXPORT double ComputeFrameworkVersionNumber;
+
+//! Project version string for ComputeFramework.
+FOUNDATION_EXPORT const unsigned char ComputeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComputeFramework/PublicHeader.h>
+
+
